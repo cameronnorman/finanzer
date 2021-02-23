d302542f249aca3d2ebcabfd346441853474a916
Cameron Norman <cameronrossnorman@gmail.com>
2021-02-23 09:52:57 +0100

    bug: fixed multi environment issue test/development

--- src/connection.ts
+++ src/connection.ts
@@ -1,25 +1,77 @@
-import { createConnection, getConnection, getRepository } from 'typeorm';
+import { ConnectionOptions, createConnection, getConnection, getRepository } from 'typeorm';
+
+interface TypeOrmCli {
+  entitiesDir: string,
+  migrationsDir: string,
+  subscribersDir: string
+}
+
+interface TypeOrmConfig {
+    synchronize: boolean,
+    logging: boolean,
+    type: string,
+    database: string,
+    entities: string[],
+    migrations: string[],
+    subscribers: string[]
+    cli: TypeOrmCli
+}
+
+interface TypeOrmEnvConfig {
+  development: ConnectionOptions,
+  test: ConnectionOptions
+}
+
+const developmentConfig: ConnectionOptions = {
+  synchronize: true,
+  logging: false,
+  type: "sqlite",
+  database: `development.sqlite`,
+  entities: ["dist/entity/**/*.js"],
+  migrations: [
+    "dist/migration/**/*.js"
+  ],
+  subscribers: [
+    "dist/subscriber/**/*.js"
+  ],
+  cli: {
+    entitiesDir: "dist/entity",
+    migrationsDir: "dist/migration",
+    subscribersDir: "dist/subscriber"
+  }
+}
+
+const testConfig: ConnectionOptions = {
+  synchronize: true,
+  logging: false,
+  type: "sqlite",
+  database: `test.sqlite`,
+  entities: ["src/entity/**/*.ts"],
+  migrations: [
+    "src/migration/**/*.ts"
+  ],
+  subscribers: [
+    "src/subscriber/**/*.ts"
+  ],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber"
+  }
+}
 
 const connection = {
-  async create(dbName: string = process.env.NODE_ENV){
-    await createConnection({
-      "synchronize": true,
-      "logging": false,
-      "type": "sqlite",
-      "database": `${dbName}.sqlite`,
-      "entities": ["src/entity/**/*.ts"],
-      "migrations": [
-        "src/migration/**/*.ts"
-      ],
-      "subscribers": [
-        "src/subscriber/**/*.ts"
-      ],
-      "cli": {
-        "entitiesDir": "src/entity",
-        "migrationsDir": "src/migration",
-        "subscribersDir": "src/subscriber"
-      }
-    })
+  async create(dbName: string = process.env.NODE_ENV) {
+    const dbEnvConfig: TypeOrmEnvConfig = {
+      development: developmentConfig,
+      test: testConfig
+    }
+
+    if (dbName === "development") {
+      await createConnection(dbEnvConfig.development)
+    } else {
+      await createConnection(dbEnvConfig.test)
+    }
   },
 
   async close(){
