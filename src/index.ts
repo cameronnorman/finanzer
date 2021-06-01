4c197d9b2ba095b9ada25ccac64f1247a11ed267
Cameron Norman <cameron.norman@visablelabs.com>
2021-06-01 10:49:49 +0200

    fix: production run

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import prisma from "../client"
 import { createServer, startServer } from "./app"
+import prisma from "./client"
 
 const server = createServer(prisma)
 startServer(server, process.env.PORT)
